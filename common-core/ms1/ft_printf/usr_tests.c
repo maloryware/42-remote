93fb4c23cf19296a03bc8fac6e01860b0aadd630
Mal <malory@onenetbeyond.org>
2025-05-20 16:10:48 +0100

    automated

--- common-core/ms1/ft_printf/usr_tests.c
+++ common-core/ms1/ft_printf/usr_tests.c
@@ -6,7 +6,7 @@
 /*   By: Mal <malory@onenetbeyond.org>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 02:25:44 by Mal               #+#    #+#             */
-/*   Updated: 2025/05/20 16:07:34 by Mal              ###   ########.fr       */
+/*   Updated: 2025/05/20 16:10:35 by Mal              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,15 @@
 
 int main()
 {
-	// something something main very muito fixe - vinny aka idfk shrug
+	// something something main very muito fixe - vinny aka idfk shrug || skibidy sigma
 	char *var = "There was a time I used to scratch my nuts and it didn't bleed";
-	printf("\n\n%-114s!!!\n", var);
-	ft_printf("\n\n%-114s!!!\n", var);
+	int vini_gostoso = 534;
+	printf("testing fucking -");
+	printf("\n\n%s!!!\n", var);
+	ft_printf("\n\n%s!!!\n", var);
+
+	printf("testing fucking +\n\n");
+	printf("%+d\n\n",  vini_gostoso);
+	ft_printf("%+d\n\n",  vini_gostoso);
+
 }
